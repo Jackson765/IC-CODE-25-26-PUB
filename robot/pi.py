3521482223760196e8e2a194a23883b71ebb542c
Jackson765 <118147028+Jackson765@users.noreply.github.com>
2025-10-23 18:10:37 -0400

    ahhh

--- robot/pi.py
+++ robot/pi.py
@@ -13,7 +13,7 @@ from readonly import RobotBase, MOTORS
 
 OPERATOR_IP = "192.168.50.150" # your laptop/pc ip address on IC2026 Network
 OPERATOR_PORT = 5600 # the port for video streaming 
-TEAM_ID = -1 # Your team ID
+TEAM_ID = 14 # Your team ID
 
 PI_IP = "192.168.50.5" # Your pi IP
 PI_PORT = 5005  # 
@@ -26,6 +26,21 @@ PURE_DC_THRESHOLD = 80
 ### Input Receiving Loop
 
 class Robot(RobotBase):
+    ### Bind Socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((PI_IP, PI_PORT))
+
+    ### Input Receiving Loop
+    inputQ = []
+    def get_input():
+        while True:
+            try:
+                data, addr = sock.recvfrom(1024) 
+                msg = json.loads(data.decode('utf-8'))
+                inputQ.append(msg)
+                print(f"[Received from {addr}] {msg}")
+            except Exception as e:
+                print("[Receiver Error]", e)
     def __init__(self, team_id):
         super().__init__(team_id)
         ### Initialization/Start Up
@@ -50,6 +65,8 @@ class Robot(RobotBase):
 
     def cleanup(self):
         pass
+    def input_loop():
+        input_thread = threading.Thread(target=input_loop, daemon=True)
 
 if __name__ == "__main__":
     team_id = 14  # your team id here
